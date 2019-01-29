e425918b91617bd79ec61acb767aa02cd5a52d03
xoreo <mattnappo@gmail.com>
2019-01-28 20:34:04 -0500

    Writing test plugins

---
+++ src/main/java/com/github/summercash/commands/TestCommand.java
@@ -0,0 +1,14 @@
+package com.github.summercash.commands;
+
+import org.bukkit.plugin.java.JavaPlugin; // Not sure if I need this just yet
+
+public class TestCommand implements CommandExecutor {
+
+    // This method is called, when somebody uses our command
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        System.out.println("console: command typed");
+        sender.sendMessage("minecraft: command worked");
+        return false;
+    }
+}
