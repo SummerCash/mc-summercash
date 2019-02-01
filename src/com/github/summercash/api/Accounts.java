efe29d3a88b55af877b8551f49139aebc035d0f5
xoreo <mattnappo@gmail.com>
2019-02-01 18:31:41 -0500

    Writing post req tests

--- src/com/github/summercash/api/Accounts.java
+++ src/com/github/summercash/api/Accounts.java
@@ -1,45 +1,59 @@
 package com.github.summercash.api;
-import org.json.simple.JSONObject;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 // import;
 
 class Accounts {
-    // private class GeneralRequest {
-    //     JSONObject request;
-
-    //     GeneralRequest(String address, String privateKey) {
-    //         request = new JSONObject();
-    //         request.put("privateKey", privateKey);
-    //         request.put("address", address);
-    //     }
-    // }
-
-    public String CallMethod(String method, String address, String privateKey) {
-        return "";
-        byte[] out = "{\"username\":\"root\",\"password\":\"password\"}" .getBytes(StandardCharsets.UTF_8);
-        int length = out.length;
-
-        HttpClient httpClient = HttpClientBuilder.create().build(); //Use this instead 
-
-        try {
-
-            HttpPost request = new HttpPost("http://yoururl");
-            StringEntity params =new StringEntity("details={\"name\":\"myname\",\"age\":\"20\"} ");
-            request.addHeader("content-type", "application/x-www-form-urlencoded");
-            request.setEntity(params);
-            HttpResponse response = httpClient.execute(request);
-
-            //handle response here...
-
-        } catch (Exception ex) {
-
-            //handle exception here
-
-        } finally {
-            //Deprecated
-            //httpClient.getConnectionManager().shutdown(); 
+    private class GeneralRequest {
+        private String data;
+        
+        public String getData() {
+        	return data;
         }
 
+        GeneralRequest(String address, String privateKey) {
+            data = "data={"
+                + "addres: " + address + ","
+                + "privateKey: " + privateKey
+            + "}";
+        }
+    }
 
+    public String CallMethod(String method, String address, String privateKey) {
+    	String requestData = new GeneralRequest("", "").getData();
+        StringEntity entity = new StringEntity(requestData, ContentType.APPLICATION_FORM_URLENCODED);
+
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost request = new HttpPost("https://google.com/");
+        request.setEntity(entity);
+
+        HttpResponse response;
+		try {
+			response = httpClient.execute(request);
+			System.out.println(response.toString());
+			System.out.println(response.getStatusLine().getStatusCode());
+			return "";
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "";
+    }
+    
+    public static void main(String[] args) {
+    	
+    	
+    }
 }
 
 
