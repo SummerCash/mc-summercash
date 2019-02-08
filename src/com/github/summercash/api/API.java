c88e25a7dfe36a6fb7b2c1d502005ca6cdd88034
xoreo <mattnappo@gmail.com>
2019-02-07 20:51:41 -0500

    Added TODO

--- src/com/github/summercash/api/API.java
+++ src/com/github/summercash/api/API.java
@@ -1,4 +1,11 @@
 package com.github.summercash.api;
+/*
+    @TODO
+    1.) Make the post requests working
+            ignore SSL cert errs
+    2.) Build the commands
+    3.) Build the database system (if necessarys)
+*/
 
 class API {
     API() {
