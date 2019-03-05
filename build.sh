f89657328355d02f8e2ae036a2db27378ee1266f
xoreo <mattnappo@gmail.com>
2019-03-04 23:35:20 -0500

    Using a mvn proj now

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-jar cf server/plugins/build.jar src/
-
---
+++ src/main/java/com/summercash/mcsummercash/plugin/SummerCash.java
@@ -0,0 +1,19 @@
+package com.summercash.mcsummercash;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import com.github.summercash.commands.TestCommand;
+
+public class SummerCash extends JavaPlugin {
+    // Fired when plugin is first enabled
+	@Override
+    public void onEnable() {
+        // Register our command "kit" (set an instance of your command class as executor)
+        this.getCommand("test").setExecutor(new TestCommand());
+    }
+    
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+        
+    }
+}
