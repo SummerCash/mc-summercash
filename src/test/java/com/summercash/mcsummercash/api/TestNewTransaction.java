a2869693e07b8df392d3032d84b95a2fbdc7ab78
xoreo <mattnappo@gmail.com>
2019-03-22 17:37:41 -0400

    Starting database

--- src/test/java/com/summercash/mcsummercash/api/TestNewTransaction.java
+++ src/test/java/com/summercash/mcsummercash/api/TestNewTransaction.java
@@ -1,11 +1,23 @@
 package com.summercash.mcsummercash.api;
 
+import com.summercash.mcsummercash.common.Common;
+
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 public class TestNewTransaction {
 
     @Test
     public void TestTransaction() {
+        NewTransaction newTransaction = new NewTransaction();
+
+        boolean completedTxn = newTransaction.Transaction(
+            Common.XoreoAddress,
+            Common.OtherAddress,
+            0.0
+        );
+
+        assertTrue("completed transaction: ", completedTxn);
 
     }
 }
